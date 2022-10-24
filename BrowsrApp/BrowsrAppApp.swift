9554d0ee3e52a8e93d0929ab838c7e9532dd74b3
Vitor Dinis <vitor.daniel@puregym.com>
2022-10-24 09:34:11 +0100

    Initial Commit

---
+++ BrowsrApp/BrowsrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowsrAppApp.swift
+//  BrowsrApp
+//
+//  Created by Vitor Dinis on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BrowsrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
